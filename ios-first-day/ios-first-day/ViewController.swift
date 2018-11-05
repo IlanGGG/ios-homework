5b879edf031b82362f8f0bc485e4b8376a2669d3
Ilan Gallula <igg.gallu@gmail.com>
2018-11-05 12:13:26 +0200

    first commit
    Signed-off-by: Ilan Gallula <igg.gallu@gmail.com>

---
+++ ios-first-day/ios-first-day/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ios-first-day
+//
+//  Created by Ilan Gallula on 05/11/2018.
+//  Copyright © 2018 Ilan Gallula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        comment
+        
+    }
+
+
+}
+
